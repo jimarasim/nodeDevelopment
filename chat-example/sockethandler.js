c065d6576afa0e3cda7b16ff236bd63515e5312d
James Arasim <jaemzware@hotmail.com>
2015-01-08 19:23:21 -0800

    sending coords in message

--- chat-example/sockethandler.js
+++ chat-example/sockethandler.js
@@ -8,13 +8,14 @@ $('#svgdiv1').click(function(event){
                     
                     $( "#span1" ).text(  pageCoords );
                     
-                    $("#svgrect1").attr("x",event.pageY);
+                    $("#svgrect1").attr("x",event.pageX);
                     $("#svgrect1").attr("y",event.pageY);
                     
-                    socket.emit('tap msg',"REMOTE:"+pageCoords);
+//                    socket.emit('tap msg',"REMOTE:"+pageCoords);
+                    socket.emit('tap msg',"{x:"+event.pageX+", y:"+event.pageY+"}");
                     });
 
 //handler for server socket io.emit, when someone taps
 socket.on('tap msg', function(msg){
-          $( "#span1" ).text(  msg );
+          $( "#span1" ).text(  msg.x );
           });
