c80c923c92311e822646d5a923826cf02f2eb968
James Arasim <jaemzware@hotmail.com>
2015-01-08 22:02:49 -0800

    rectangle interaction

--- chat-example/sockethandler.js
+++ chat-example/sockethandler.js
@@ -3,19 +3,29 @@
 var socket = io();
 
 $('#svgdiv1').click(function(event){
-                    var pageCoords = "( event.pageX, event.pageY ) : " + "( " + event.pageX + ", " + event.pageY + " )";
-                    var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
                     
-                    $( "#span1" ).text(  pageCoords );
+                    //get coordinates of the point clicked
+                    var clickPointX = event.pageX;
+                    var clickPointY = event.pageY;
                     
-                    $("#svgrect1").attr("x",event.pageX);
-                    $("#svgrect1").attr("y",event.pageY);
-                    
-//                    socket.emit('tap msg',"REMOTE:"+pageCoords);
-                    socket.emit('tap msg',"{x:"+event.pageX+", y:"+event.pageY+"}");
+                    //report coordinates to the server
+                    socket.emit('tap msg','{"x":"'+event.pageX+'", "y":"'+event.pageY+'"}');
+
                     });
 
 //handler for server socket io.emit, when someone taps
 socket.on('tap msg', function(msg){
-          $( "#span1" ).text(  msg.x );
-          });
+          
+                    //convert json string to an object
+                    var msgObject = jQuery.parseJSON(msg);
+          
+                    //get the coordinates reported
+                    var setPointX = msgObject.x;
+                    var setPointY = msgObject.y;
+          
+                    //move the rectangle to where the click was made
+                    $("#svgrect1").attr("x",setPointX);
+                    $("#svgrect1").attr("y",setPointY);
+          
+                    $( "#span1" ).text( "BROADCASTED:"+ msg + "X:"+msgObject.x+" Y:"+msgObject.y);
+                    });
