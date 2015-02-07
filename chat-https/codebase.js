6139a44914f7ec8f11bde95bb0fd596bc9dbfd2c
jimarasim <jaemzware@hotmail.com>
2015-02-07 12:37:02 -0800

    starting https chat so it can be loaded and not blocked

---
+++ chat-https/codebase.js
@@ -0,0 +1,50 @@
+
+//WHEN THE PAGE IS DONE LOADING
+document.addEventListener('DOMContentLoaded', function () {
+                          
+                          SetupEvents();
+                          
+                          });
+
+
+//DEFINE EVENTS TO RESPOND TO 71ck3t#Fly
+function SetupEvents()
+{
+    $("#svgtag1").mousemove(function(event){Svg1MouseMove(event);});
+    $("#svgtag4").click(function(){Svg4Clicked();});
+    
+}
+
+
+//WHEN THE FIRST SVG (WITH THE BLUE SQUARE) IS CLICKED
+function Svg1MouseMove(event){
+    var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+    var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+    $( "#span1" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+    $( "#span2" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
+}
+
+//WHEN THE SVG WITH AN IMAGE IS CLICKED
+function Svg4Clicked(){
+    if($("#svgimage1").attr("x")==0){
+        $("#svgimage1").attr("x",10);
+    }
+    else{
+        $("#svgimage1").attr("x",0);
+    }
+}
+
+//DRAG AND DROP SUPPORT
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
---
+++ chat-https/index.js
@@ -0,0 +1,78 @@
+//http://socket.io/get-started/chat/
+//setup an express application and bind it to an http server
+var app = require('express')();
+
+//require express for serving other, static files, like .css from the root dir
+var express=require('express');
+
+//require http
+var http = require('http').Server(app);
+
+//attach a socket to the listening http
+var io = require('socket.io')(http);
+
+//include static files like .css
+app.use(express.static(__dirname));
+
+////////////////////////////////////////////////////GET REQUEST HANDLERS
+//handler for incoming get requests
+app.get('/', function(req, res){
+        //send a file back as the response
+        res.sendFile(__dirname + '/index.html');
+});
+
+//handler for incoming get requests
+app.get('/canvas', function(req, res){
+        //send a file back as the response
+        res.sendFile(__dirname + '/canvas.html');
+        });
+
+//handler for incoming get requests
+app.get('/svg', function(req, res){
+        //send a file back as the response
+        res.sendFile(__dirname + '/svg.html');
+        });
+////////////////////////////////////////////////////GET REQUEST HANDLERS
+
+////////////////////////////////////////////////////SOCKET HANDLERS
+//handler for incoming socket connections
+io.on('connection', function(socket){
+      console.log('a user connected');
+      
+      //specify the socket disconnect event handler to print out a message when the user disconnects
+      socket.on('disconnect', function(){
+                console.log('user disconnected');
+                });
+      
+      //specify 'chat message' event handler. happens when emit(ted) by the client
+      socket.on('chat message', function(chatClientMessage){
+          
+                //get the address of the message emitter
+                var chatClientAddress = socket.handshake.address;
+                var chatClientDate = new Date();
+                
+          
+                console.log('chatClientMessage: ' + chatClientMessage);
+                
+                
+                
+                //broadcast chat message (client page needs to have  a socket.on handler for this)
+                io.emit('chat message','{"chatClientMessage":"'+chatClientMessage+'","chatClientAddress":"'+chatClientAddress+'","chatClientDate":"'+chatClientDate.toUTCString()+'"}');
+
+                   
+                });
+      
+      //specify 'tapmessage' event handler, when emit(ted) by the user connection
+      socket.on('tap msg', function(msg){
+                console.log('message: ' + msg);
+                
+                //broadcast chat message (client page needs to have  a socket.on handler for this)
+                io.emit('tap msg',msg);
+                });
+});
+////////////////////////////////////////////////////SOCKET HANDLERS
+
+http.listen(3000, function(){
+            console.log('listening on *:3000');
+});
+
