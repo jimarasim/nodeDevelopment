a566626e4fc6edaec0e7a77b7e8bb5f02dfe6491
jimarasim <jaemzware@hotmail.com>
2015-02-24 21:51:00 -0800

    removed stuff not being used

--- peightyone.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-  	response.writeHead(200, {'Content-Type': 'text/html'});
-  	response.write('<h1>jaemzware</h1>');
-	response.end('\n');
-}).listen(81);
-
-console.log('Server running at http://localhost:81/');
--- webserver-html.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('<html><head><title>jaemzware</title></head><body><h1>jaemzware</h1></body></html>');
-  response.end('\n');
-}).listen(80);
-
-console.log('Server running at http://localhost:80/');
