12195925211eb0be3caba6ce724493b2c774dfe9
JAMES ARASIM <jaemzware@hotmail.com>
2015-07-05 08:04:17 -0700

    push from sk8creteordiebook

--- chat-example/sockethandler.js
+++ chat-example/sockethandler.js
@@ -77,9 +77,9 @@ socket.on('chatmessage', function(msgObject){
             }
             else if(chatClientMessage.indexOf(".mp3") > 0){
                 //change the source of the media player
-                $('#changeableaudiosource').attr("src",chatClientMessage);
-                document.getElementById("changeableaudiosource").load();
-                document.getElementById("changeableaudiosource").play();
+                $('#jaemzwaredynamicaudiosource').attr("src",chatClientMessage);
+                document.getElementById("jaemzwaredynamicaudioplayer").load();
+                document.getElementById("jaemzwaredynamicaudioplayer").play();
             }
             else if(chatClientMessage.includes(".mp4")){
                 var videoId = "video"+chatServerDate;
