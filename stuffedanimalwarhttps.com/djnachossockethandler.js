75c2cf32c2fb58f84000f6355d5aa6125e25e575
jimarasim <jaemzware@hotmail.com>
2015-08-13 17:22:41 -0700

    starting the https version

---
+++ stuffedanimalwarhttps.com/djnachossockethandler.js
@@ -0,0 +1,215 @@
+/* STUFFED ANIMAL WAR - jaemzware.org - 2015*/
+/* THIS SCRIPT NEEDS TO BE INCLUDED AFTER THE ELEMENTS REFERENCED ARE DEFINED ON THE PAGE */
+var masterAlias = "DJ NACHOS";
+var unspecifiedAlias = "[______]";
+var socket = io();
+var stuffedanimalwardivTop = 0; 
+
+//CONTAINS METHOD
+String.prototype.contains = function(it) { return this.indexOf(it) !== -1; };
+
+//CHAT MESSAGE HANDLER - CHAT MESSAGE => SOCKET (COMMON)
+$('form').submit(function(){
+
+    //GET THE MESSAGE IN THE MESSAGE BOX
+    var chatMessage = $('#chatClientMessage').val();
+
+    //CLEAR THE MESSAGE FROM THE MESSAGE BOX
+    $('#chatClientMessage').val('');
+
+    console.log("CALLING EMITCHATMESSAGE FROM FORM SUBMIT WITH #chatClientMessage => "+chatMessage);
+    emitChatMessage(chatMessage);
+
+    return false;
+});
+
+//AUTORESPONDER HANDLER - SELECT DROP DOWN (COMMON) 
+$('#chatClientAutoResponder').change(function(){
+
+    //GET THE MESSAGE FROM THE AUTORESPONDER
+    var chatMessage = $('#chatClientAutoResponder option:selected').text();
+
+    console.log("CALLING EMITCHATMESSAGE FROM AUTORESPONDER WITH #chatClientAutoResponder option:selected => "+chatMessage);
+    emitChatMessage(chatMessage);
+
+    //set the autoresponder back to blanck
+    $('#chatClientAutoResponder').val('blank');
+});
+
+//SONGS HANDLER - SELECT DROP DOWN - CHANGE SONG (COMMON)
+$('#selectsongs').change(function(){
+    var songToPlay = $('#selectsongs option:selected').attr("value");
+
+    emitChatMessage(songToPlay);
+});
+
+    //VIDEOS - CHANGE VIDEO (COMMON)
+    $('#selectvideos').change(function(){
+        var videoToPlay = $('#selectvideos option:selected').attr("value");
+
+        emitChatMessage(videoToPlay);
+    });
+
+//EMITCHATMESSAGE - CALLED BY CHAT MESSAGE FORM SUBMIT AND AUTORESPONDER (UNCOMMON, CALLS UNIQUE SOCKET.EMIT CALLBACK
+function emitChatMessage(message){
+    //get the user alias
+    var chatClientUser = $("#chatClientUser").val();
+    
+    //SET THE DEFAULT ALIAS IF IT'S EMPTY
+    if(chatClientUser.length===0){
+        chatClientUser = unspecifiedAlias;
+    }
+
+    //CONSTRUCT THE MESSAGE TO EMIT IN JSON, WITH THE USERNAME INCLUDED
+    var chatMessageObject = {
+              CHATCLIENTUSER: chatClientUser,
+              CHATSERVERUSER:'',
+              CHATCLIENTMESSAGE:message,
+              CHATSERVERDATE:''
+          }  
+    
+    //send the message
+    socket.emit('chatmessage',chatMessageObject); 
+}
+
+//CHAT MESSAGE: SOCKET => CHAT MESSAGES - DJ NACHOS (chatmessage) - ALL COMMON EXCEPT FOR JUST THE EVENT NAME, WHICH NEEDS TO BE UNIQUE TO SERVICE DIFFERENT CHAT PAGES ON THE SAME SERVER
+socket.on('chatmessage', function(msgObject){
+    var chatClientUser = msgObject.CHATCLIENTUSER;
+    var chatServerUser = msgObject.CHATSERVERUSER;
+    var chatClientMessage = msgObject.CHATCLIENTMESSAGE;
+    var chatServerDate = msgObject.CHATSERVERDATE;
+    var serverStamp = "[IP:"+chatServerUser+" DATE:"+chatServerDate+"]"; //ip and time stamp
+    
+    console.log("RECEIVED MSGOBJECT FROM A CHATMESSAGE BROADCAST:"+JSON.stringify(msgObject));
+    console.log("PARAMETERIZED FOR READABILITY CHATCLIENTUSER:"+chatClientUser+" CHATSERVERUSER:"+chatServerUser+" CHATCLIENTMESSAGE:"+chatClientMessage+" CHATSERVERDATE:"+chatServerDate);
+
+    //smart link - recognize chat links (only at the very beginning of the message), and display them appropriately.
+    if (
+        chatClientMessage.indexOf("http://")===0||
+        chatClientMessage.indexOf("https://")===0
+       ){ 
+            if( chatClientMessage.indexOf(".jpg")   >   0 ||
+                chatClientMessage.indexOf(".jpeg")  >   0 ||
+                chatClientMessage.indexOf(".gif")   >   0 ||
+                chatClientMessage.indexOf(".png")   >   0)
+            {
+
+                //show the image if it's just an image tag
+                $('#messagesdiv').prepend($('<br />'));
+                $("<img/>").prependTo("#messagesdiv").attr({
+                    src: chatClientMessage,
+                    alt: chatClientUser+" "+chatServerUser+" "+chatClientMessage+" "+chatServerDate,
+                 });
+            }
+          else if(chatClientMessage.indexOf(".mp3") > 0 )
+//                      &&
+//                  chatClientUser===masterAlias)
+            {
+                //change the source of the AUDIO player
+                $('#jaemzwaredynamicaudiosource').attr("src",chatClientMessage);
+                document.getElementById("jaemzwaredynamicaudioplayer").load();
+                document.getElementById("jaemzwaredynamicaudioplayer").play();
+            }
+          else if(chatClientMessage.includes(".mp4"))
+//                      &&
+//                   chatClientUser===masterAlias)
+            {
+                //change the source of the VIDEO player
+                $('#jaemzwaredynamicvideosource').attr("src",chatClientMessage);
+                document.getElementById("jaemzwaredynamicvideoplayer").load();
+                document.getElementById("jaemzwaredynamicvideoplayer").play();
+            }
+            else{
+                $('#messagesdiv').prepend($('<br />'));
+                
+                //ip and time stamp
+                $("<span>").prependTo("#messagesdiv").attr({
+                   class: "serverdate"
+                }).text(serverStamp);
+                
+                //user alias
+                $("<span>").prependTo("#messagesdiv").attr({
+                                    class: "chatclientuser"
+                                 }).text(chatClientUser);
+
+                 //chat message
+                $("<span>").prependTo("#messagesdiv").attr({
+                   class: "chatclientmessage"
+                }).text(chatClientMessage);
+                
+            }
+        }
+        else{
+            $('#messagesdiv').prepend($('<br />'));
+            
+            //ip and time stamp
+            $("<span>").prependTo("#messagesdiv").attr({
+               class: "serverdate"
+            }).text(serverStamp);
+
+            //user alias
+            $("<span>").prependTo("#messagesdiv").attr({
+                                class: "chatclientuser"
+                             }).text(chatClientUser);
+
+             //chat message
+            $("<span>").prependTo("#messagesdiv").attr({
+               class: "chatclientmessage"
+            }).text(chatClientMessage);
+        }
+});
+
+//STUFFED ANIMAL WAR (COMMON)
+//tell server about new coordinates when clicked
+$('#stuffedanimalwarsvg').click(function(event){
+    console.log('CLICKSTUFFEDANIMALWARSVG tapmsg','{"x":"'+event.pageX+'", "y":"'+event.pageY+'"}');
+    //report coordinates to the server
+    socket.emit('tapmsg','{"x":"'+event.pageX+'", "y":"'+event.pageY+'"}');
+});
+
+//STUFFED ANIMAL WAR - DJ NACHOS (tapmsg) - ALL COMMON EXCEPT FOR JUST THE EVENT NAME, WHICH NEEDS TO BE UNIQUE TO SERVICE DIFFERENT CHAT PAGES ON THE SAME SERVER
+//the server is telling us new coordinates for the next line from another player
+//DRAW A LINE DEPENDING ON COORDINATES RECEIVED FROM THE SERVER SIDE JAVASCRIPT
+socket.on('tapmsg', function(msg){
+          
+    //width of the line to draw
+    var lineWidth = 5;
+          
+    //convert json string to an object
+    var msgObject = jQuery.parseJSON(msg);
+
+    //get the coordinates emitted
+    var newPointX = msgObject.x;
+    var newPointY = msgObject.y;
+
+    //save off these coordinates (for drawing a line)
+    var oldPointX =$("#stuffedanimalwarsvgrect").attr("x");
+    var oldPointY =$("#stuffedanimalwarsvgrect").attr("y");
+
+    //draw a line from the new to the old location
+    var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+    
+    newLine.setAttribute('id','line'+$.now());
+    newLine.setAttribute('x1',newPointX);
+    newLine.setAttribute('y1',newPointY-stuffedanimalwardivTop); //NOTE: TOP OF .stuffedanimalwardivTop IS defined above
+    newLine.setAttribute('x2',oldPointX);
+    newLine.setAttribute('y2',oldPointY);
+    newLine.setAttribute('style','stroke:rgb('+getRandomColorValue()+','+getRandomColorValue()+','+getRandomColorValue()+');stroke-width:'+lineWidth); //RANDOM COLOR
+//    newLine.setAttribute('style','stroke:rgb(0,0,0);stroke-width:'+lineWidth); //BLACK LINE
+
+    $("#stuffedanimalwarsvg").append(newLine);
+    
+    //move the rectangle to where the click was made
+    $("#stuffedanimalwarsvgrect").attr("x",newPointX);
+    $("#stuffedanimalwarsvgrect").attr("y",newPointY-stuffedanimalwardivTop); 
+
+});
+
+
+/* GETRANDOMCOLORVALUE (COMMON)
+ * this function returns a random color value, used by drawing new things
+ */
+function getRandomColorValue(){
+    return Math.floor((Math.random() * 255) + 1);
+}
+
---
+++ stuffedanimalwarhttps.com/tabstripvideo.js
@@ -0,0 +1,164 @@
+/* PAGE HAS FINISHED LOADING, SETUP PAGE EVENTS */
+document.addEventListener('DOMContentLoaded', function () {
+    SetupEvents();
+});
+
+
+function SetupEvents(){
+    
+    //SETUP VIDEO STRIP-LIKE VIDEO PLAYER
+    if($('span[id*="jaemzwarevideo"]').length>0){
+        //GET IDS OF ALL THE VIDEOS
+        var multiGroup=[];
+        $('span[id*="jaemzwarevideo"]').each(function(index, element){
+                                               multiGroup[index]=$(element).attr('id');
+                                               });
+        //CREATE VIDEO PLAYERS WITH THOSE IDS
+        MultiVideoJquery(multiGroup);
+    }
+}
+    
+//CURRENT JQUERY WAY:
+//1. PRECEDE GROUPE NAME WITH "jaemzwarevid"
+//1. ADD ARRAY ASSIGNMENT FOR GROUP NAME e.g. jaemzwarevideojimroxhill20150802 TO ELSE IF BELOW
+//MEDIA URL[0]  (REQUIRED: MP4, OGV*, PNG), TITLE[1]
+//*removed for this implementation
+
+var jaemzwarevideoadam20150207 = [
+                    ["https://blackmarketskates.com/media/marginaladam/IMG_0682","Adam Marginal rocking"],
+                    ["https://blackmarketskates.com/media/marginaladam/IMG_0684","Adam Marginal cradle"],
+                    ["https://blackmarketskates.com/media/marginaladam/IMG_0686","Adam Marginal lipping"],
+                    ["https://blackmarketskates.com/media/marginaladam/IMG_0688","Adam Marginal sliding"]
+                    ];
+
+var jaemzwarevideojimroxhill20150802 = [
+                    ["http://www.helena.seattlerules.com/media/jim/IMG_3263","Jim Roxhill Rox bank and bank hip"],
+                    ["http://www.helena.seattlerules.com/media/jim/IMG_3264","Jim Roxhill Rox bank and step-up"],
+                    ["http://www.helena.seattlerules.com/media/jim/IMG_3272","Jim Roxhill small backside ollie line"],
+                    ["http://www.helena.seattlerules.com/media/jim/IMG_3273","Jim Roxhill board slide"]
+                    ];
+                    
+                    
+
+/* this function writes the video player and individual videos from jquery */
+function MultiVideoJquery(multiGroup){
+    
+    //write out all the ids found
+    for(i=0;i<multiGroup.length;i++){
+        
+        //get the array of video file paths (sans extension) from the pre-built array
+        if(multiGroup[i]==="jaemzwarevideojimroxhill20150802"){
+            filePathsNoExtensionArray = jaemzwarevideojimroxhill20150802;
+        }
+        else if(multiGroup[i]==="jaemzwarevideoadam20150207"){
+            filePathsNoExtensionArray = jaemzwarevideoadam20150207;
+        }
+        else{
+            console.error("UNEXPECTED JAEMZWAREVIDEOGROUP PRE-BUILT VIDEO ARRAY");
+        }
+        
+        //write out the video player
+        if(filePathsNoExtensionArray.length>0)
+        {
+            //write the video player
+            $("#"+multiGroup[i]).after("<video style='width: 100%;' controls='controls' preload='metadata' poster='"+filePathsNoExtensionArray[0][0]+".png' title='"+filePathsNoExtensionArray[0][1]+"' id='"+multiGroup[i]+"Video'>browser doesn't support video</video>");
+            
+            //add the different file format clips
+            $("#"+multiGroup[i]+"Video").append("<source src='"+filePathsNoExtensionArray[0][0]+".mp4' type='video/mp4' id='"+multiGroup[i]+"Videomp4'/>");
+            
+            //write out the video strip
+            $("#"+multiGroup[i]+"Video").before("<span class='videolinktext' id='"+multiGroup[i]+"VideoTitle'>"+filePathsNoExtensionArray[0][1]+"</span>");
+            $("#"+multiGroup[i]+"VideoTitle").after("<div class='videostrip' id='"+multiGroup[i]+"VideoStripDiv' class='videostrip'></div>");
+            $("#"+multiGroup[i]+"VideoStripDiv").append("<table id='"+multiGroup[i]+"VideoStripTable'></table>");
+            $("#"+multiGroup[i]+"VideoStripTable").append("<tr id='"+multiGroup[i]+"VideoStripTR'></tr>");
+
+            
+            //WRITE OUT THE FIRST VIDEO
+            $("#"+multiGroup[i]+"VideoStripTR").append("<td id='"+multiGroup[i]+"VideoStripTD0'></td>");
+            $("#"+multiGroup[i]+"VideoStripTD0").append("<img class='videolink' id='"+multiGroup[i]+"VideoStripIMG0' \
+                                                       title='"+filePathsNoExtensionArray[0][1]+"' \
+                                                       alt='"+filePathsNoExtensionArray[0][1]+"' \
+                                                       src='"+filePathsNoExtensionArray[0][0]+".png' \
+                                                       onclick='MultiVideoJqueryChanger(\""+
+                                                                    filePathsNoExtensionArray[0][0]+".png\",\""+
+                                                                    filePathsNoExtensionArray[0][0]+".mp4\",\""+
+                                                                    multiGroup[i]+"\",\""+
+                                                                    filePathsNoExtensionArray[0][1]+"\")' \
+                                                       />");
+            
+            
+            
+            console.log('CHECK STRIP WITH ONE VIDEO');
+            
+            
+            //WRITE OUT THE REST OF THE VIDEOS
+            for (var j=1;j<filePathsNoExtensionArray.length;j++)
+            {
+                $("#"+multiGroup[i]+"VideoStripTR").append("<td id='"+multiGroup[i]+"VideoStripTD"+j+"'></td>");
+                $("#"+multiGroup[i]+"VideoStripTD"+j).append("<img class='videolink' id='"+multiGroup[i]+"VideoStripIMG"+j+"' \
+                                                           title='"+filePathsNoExtensionArray[j][1]+"' \
+                                                           alt='"+filePathsNoExtensionArray[j][1]+"' \
+                                                           src='"+filePathsNoExtensionArray[j][0]+".png' \
+                                                           onclick='MultiVideoJqueryChanger(\""+filePathsNoExtensionArray[j][0]+".png\",\""+
+                                                                                                filePathsNoExtensionArray[j][0]+".mp4\",\""+
+                                                                                                multiGroup[i]+
+                                                                                                "\",\""+filePathsNoExtensionArray[j][1]+"\")' \
+                                                           />");
+                
+            }
+            
+            console.log('CHECK STRIP WITH ALL VIDEOS');
+            
+            
+        }
+        else{
+            $("#"+multiGroup[i]).text("unexpected no filePathsNoExtensionArray, or no entries:"+filePathsNoExtensionArray);
+        }
+        
+    }
+}
+
+/**
+ * This function changes the video loaded in the video player for video pages
+ * @param {type} basepath - if empty, assumes the whole path is specified
+ * @param {type} png - full path to the png image
+ * @param {type} mp4 - full path to the mp4 file
+ * @param {type} ogv - full path to the ogv file
+ * @param {type} group - group of videos / images (array) to change. need to put this in the id
+ * of the video images and video, so there could be more than one on a page
+ * @returns {undefined}
+ */
+function ChangeVideo(basepath,png,mp4,ogv,group,fileNameOnly)
+{
+    var pngpath = basepath===""?png:basepath + png;
+    var mp4path = basepath===""?mp4:basepath + mp4;
+    
+    //set elements of the video
+    document.getElementById(group+"videopng").setAttribute("poster",pngpath);
+    document.getElementById(group+"videomp4").setAttribute("src",mp4path);
+    
+    //set element of the video title
+    document.getElementById(group+"videoTitle").innerHTML=fileNameOnly;
+    
+    //reload the video
+    document.getElementById(group+"videopng").load();
+}
+
+
+function MultiVideoJqueryChanger(png,mp4,group,title)
+{
+    //set elements of the video
+    document.getElementById(group+"Video").setAttribute("poster",png);
+    document.getElementById(group+"Video").setAttribute("title",title);
+    document.getElementById(group+"Videomp4").setAttribute("src",mp4);
+    
+    //set element of the video title
+    document.getElementById(group+"VideoTitle").innerHTML=title;
+    
+    //reload the video
+    document.getElementById(group+"Video").load();
+}
+
+
+
+
