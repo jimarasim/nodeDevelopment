79ad453b59773c05e1fdce371e2b5499b751bd56
JAMES ARASIM <jaemzware@hotmail.com>
2015-08-23 12:26:54 -0700

    push from sk8creteordiebook

--- stuffedanimalwar.com/htmlwriter.js
+++ stuffedanimalwar.com/htmlwriter.js
@@ -35,7 +35,7 @@ function writeChatControls(){
     document.write("<input id=\"chatClientUser\" placeholder=\"your alias\"/>");
     document.write("<input id=\"chatClientMessage\" size=\"35\" placeholder=\"type message\" />");
     document.write("<select id=\"chatClientAutoResponder\" size=1 >");
-    writeAutoResponderOptions();
+    writeDefaultAutoResponderOptions();
     document.write("</select>");
     document.write("<button>SEND</button>");
     document.write("</form>");
@@ -45,74 +45,75 @@ function writeChatMessagesDiv(){
     document.write("<div id=\"messagesdiv\"></div>");
 }
 
-//WRITECHAT - this function writes the Stuffed Animal War style chat  to the page
+//WRITECHAT
 function writeChat(){
     document.write("<table>");
-        document.write("<tr>");
-        document.write("<td>");
-        writeChatControls();
-        document.write("</td>");
-        document.write("</tr>");
-        document.write("<tr>");
-        document.write("<td>");
-        writeChatMessagesDiv();
-        document.write("</td>");
-        document.write("</tr>");
+    document.write("<tr>");
+    document.write("<td>");
+    writeChatControls();
+    document.write("</td>");
+    document.write("</tr>");
+    document.write("<tr>");
+    document.write("<td>");
+    writeChatMessagesDiv();
+    document.write("</td>");
+    document.write("</tr>");
     document.write("</table>");
 }
 
-function writeAutoResponderOptions(){
+//WRITEDEFAULTAUTORESPONDEROPTIONS
+function writeDefaultAutoResponderOptions(){
     document.write("<option value=\"blank\" selected>--autoresponse--</option>");
-                document.write("<option value=\"wink\">;)</option>");
-                document.write("<option value=\"smiley\">:)</option>");
-                document.write("<option value=\"bigsmiley\">:D</option>");
-                document.write("<option value=\"didyougoogle\">did you google that comeback?</option>");
-                document.write("<option value=\"idk\">i dont know</option>");
-                document.write("<option value=\"ikr\">i know right</option>");
-                document.write("<option value=\"ilikeyou\">i like you</option>");
-                document.write("<option value=\"iloveyou\">i love you</option>");
-                document.write("<option value=\"ithinkyoure\">i think youre attractive</option>");
-                document.write("<option value=\"holdmybeerwatchthis\">hold my beer, watch this</option>");
-                document.write("<option value=\"idratherplaywithmycat\">id rather play with my cat</option>");
-                document.write("<option value=\"ilikeyou\">i like you</option>");
-                document.write("<option value=\"linkisbusted\">that link is busted/option>");
-                document.write("<option value=\"look it up\">look it up/option>");
-                document.write("<option value=\"lol\">LOL</option>");
-                document.write("<option value=\"nice\">nice</option>");
-                document.write("<option value=\"nuhuh\">Nuh UH!</option>");
-                document.write("<option value=\"ok\">ok</option>");
-                document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
-                document.write("<option value=\"sarcastic\">sarcastic clap</option>");
-                document.write("<option value=\"silence\">silence for effect</option>");
-                document.write("<option value=\"vague\">vague hipster comment</option>");
-                document.write("<option value=\"witty\">witty comeback</option>");
-                document.write("<option value=\"omg\">OMG</option>");
-                document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
-                document.write("<option value=\"stfu\">STFU</option>");
-                document.write("<option value=\"thatlinkisrad\">that link is rad</option>");
-                document.write("<option value=\"thatscool\">thats cool</option>");
-                document.write("<option value=\"thatsucks\">that sucks</option>");
-                document.write("<option value=\"thatscool\">thats cool</option>");
-                document.write("<option value=\"thatswhatshesaid\">thats what she said</option>");
-                document.write("<option value=\"whatever\">whatever</option>");
-                document.write("<option value=\"youinspireme\">you inspire me</option>");
-                document.write("<option value=\"youlookverynicetoday\">you look very nice today</option>");
-                document.write("<option value=\"yourock\">you rock</option>");
-                document.write("<option value=\"youropinioniswrong\">your opinion is wrong</option>");
-                document.write("<option value=\"yousuck\">you suck</option>");
-                document.write("<option value=\"youknowyourearealupperson\">youre a real UP person</option>");
-                document.write("<option value=\"youremybestfriend\">youre my best friend</option>");
-                document.write("<option value=\"youreworkingthatoutfitgirl\">youre working that outfit girl</option>");
-                document.write("<option value=\"yourewrong\">youre wrong</option>");
+    document.write("<option value=\"wink\">;)</option>");
+    document.write("<option value=\"smiley\">:)</option>");
+    document.write("<option value=\"bigsmiley\">:D</option>");
+    document.write("<option value=\"didyougoogle\">did you google that comeback?</option>");
+    document.write("<option value=\"idk\">i dont know</option>");
+    document.write("<option value=\"ikr\">i know right</option>");
+    document.write("<option value=\"ilikeyou\">i like you</option>");
+    document.write("<option value=\"iloveyou\">i love you</option>");
+    document.write("<option value=\"ithinkyoure\">i think youre attractive</option>");
+    document.write("<option value=\"holdmybeerwatchthis\">hold my beer, watch this</option>");
+    document.write("<option value=\"idratherplaywithmycat\">id rather play with my cat</option>");
+    document.write("<option value=\"ilikeyou\">i like you</option>");
+    document.write("<option value=\"linkisbusted\">that link is busted/option>");
+    document.write("<option value=\"look it up\">look it up/option>");
+    document.write("<option value=\"lol\">LOL</option>");
+    document.write("<option value=\"nice\">nice</option>");
+    document.write("<option value=\"nuhuh\">Nuh UH!</option>");
+    document.write("<option value=\"ok\">ok</option>");
+    document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
+    document.write("<option value=\"sarcastic\">sarcastic clap</option>");
+    document.write("<option value=\"silence\">silence for effect</option>");
+    document.write("<option value=\"vague\">vague hipster comment</option>");
+    document.write("<option value=\"witty\">witty comeback</option>");
+    document.write("<option value=\"omg\">OMG</option>");
+    document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
+    document.write("<option value=\"stfu\">STFU</option>");
+    document.write("<option value=\"thatlinkisrad\">that link is rad</option>");
+    document.write("<option value=\"thatscool\">thats cool</option>");
+    document.write("<option value=\"thatsucks\">that sucks</option>");
+    document.write("<option value=\"thatscool\">thats cool</option>");
+    document.write("<option value=\"thatswhatshesaid\">thats what she said</option>");
+    document.write("<option value=\"whatever\">whatever</option>");
+    document.write("<option value=\"youinspireme\">you inspire me</option>");
+    document.write("<option value=\"youlookverynicetoday\">you look very nice today</option>");
+    document.write("<option value=\"yourock\">you rock</option>");
+    document.write("<option value=\"youropinioniswrong\">your opinion is wrong</option>");
+    document.write("<option value=\"yousuck\">you suck</option>");
+    document.write("<option value=\"youknowyourearealupperson\">youre a real UP person</option>");
+    document.write("<option value=\"youremybestfriend\">youre my best friend</option>");
+    document.write("<option value=\"youreworkingthatoutfitgirl\">youre working that outfit girl</option>");
+    document.write("<option value=\"yourewrong\">youre wrong</option>");
 }
 
-//WRITEAUDIOPLAYER - this function writes the Stuffed Animal War style audio player to the page
+//WRITEAUDIOPLAYER 
 function writeAudioPlayer(){
 document.write("<table>");
     document.write("<tr>");
         document.write("<td>");
             document.write("<audio id=\"jaemzwaredynamicaudioplayer\" controls=\"\" preload=\"none\">");
-                document.write("<source id=\"jaemzwaredynamicaudiosource\" src=\"http://jaemzware.com/analogarchive/analog/01%20Would_.mp3\" type=\"audio/mpeg\">");
+                document.write("<source id=\"jaemzwaredynamicaudiosource\" src=\"http://analogarchive.com/analogarchive/live/cracksabbath20140509-02.mp3\" type=\"audio/mpeg\">");
                 document.write("HTML5 Audio Tag support not available with your browser. For source type='audio/mpeg'");
             document.write("</audio>");
         document.write("</td>");
@@ -127,11 +128,10 @@ document.write("<table>");
 document.write("</table>");       
 }
 
-//WRITEAUDIOPLAYERDROPDOWNSONGS - this function writes the song options for the Stuffed Animal War style audio player. 
-//                                originally just called from writeAudioPlayer
+//WRITEAUDIOPLAYERDROPDOWNSONGS 
 function writeAudioPlayerDropdownSongs()
 {
-    document.write("<option selected value=\"\"></option>");
+    document.write("<option selected value=\"blank\">--pick a song from seattle--</option>");
     document.write("<option value=\"http://analogarchive.com/analogarchive/live/cracksabbath20140509-02.mp3\">crack sabbath-May 9th, 2015-02</option>");
     document.write("<option value=\"http://analogarchive.com/analogarchive/live/cracksabbath20140509-03.mp3\">crack sabbath-May 9th, 2015-03</option>");
     document.write("<option value=\"http://analogarchive.com/analogarchive/live/cracksabbath20140509-04.mp3\">crack sabbath-May 9th, 2015-04</option>");
@@ -243,17 +243,15 @@ function writeVideoPlayer(){
     document.write("</tr>");
     document.write("<td>");
     document.write("<select id=\"selectvideos\">");
-
     writeVideoPlayerDropdownVideos();
-    
     document.write("</select>");
     document.write("</td>");
     document.write("</tr>");
     document.write("</table>");
 }
 
-function writeVideoPlayerDropdownVideos(){
-    document.write("<option value=\"http://jaemzware.org/media/skate/roxhillbacksideollieline201508.mp4\">Jim, Roxhill, Backside Ollie Line,  August 2015</option>");
+function writeVideoPlayerDefaultVideos(){
+    document.write("<option selected value=\"http://jaemzware.org/media/skate/roxhillbacksideollieline201508.mp4\">Jim, Roxhill, Backside Ollie Line,  August 2015</option>");
     document.write("<option value=\"http://jaemzware.org/media/skate/roxhillollierockline201508.mp4\">Jim, Roxhill Ollie Rock Line</option>");
     document.write("<option value=\"http://jaemzware.org/media/skate/yakima201508.mp4\">Jim, Yakima August 2015</option>");
     document.write("<option value=\"http://jaemzware.org/media/randommp4/01goingagainstyourmind.mp4\">Built to Spill - Going against your mind - Fremont Fair 2014</option>");
@@ -274,12 +272,16 @@ function writeVideoPlayerWithChat(){
     document.write("</tr>");
     document.write("<td>");
     document.write("<select id=\"selectvideos\">");
-
-    writeVideoPlayerDropdownVideos();
-    
+    writeVideoPlayerDefaultVideos();
     document.write("</select>");
     document.write("</td>");
-    document.write("<td></td>");
+    writeChatControls();
+    document.write("</td>");
+    document.write("</tr>");
+    document.write("<tr>");
+    document.write("<td>");
+    writeChatMessagesDiv();
+    document.write("</td>");
     document.write("</tr>");
     document.write("</table>");
 }
