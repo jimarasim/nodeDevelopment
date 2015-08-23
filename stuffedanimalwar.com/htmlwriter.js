003c98b556c404bd1e2e32118f389a77b54894f9
JAMES ARASIM <jaemzware@hotmail.com>
2015-08-23 12:15:45 -0700

    push from sk8creteordiebook

--- stuffedanimalwar.com/htmlwriter.js
+++ stuffedanimalwar.com/htmlwriter.js
@@ -19,48 +19,7 @@ document.write("<table>");
                 document.write("<input id=\"chatClientUser\" placeholder=\"your alias\"/>");
                 document.write("<input id=\"chatClientMessage\" size=\"35\" placeholder=\"type message\" />");
                 document.write("<select id=\"chatClientAutoResponder\" size=1 >");
-                document.write("<option value=\"blank\" selected>--autoresponse--</option>");
-                document.write("<option value=\"wink\">;)</option>");
-                document.write("<option value=\"smiley\">:)</option>");
-                document.write("<option value=\"bigsmiley\">:D</option>");
-                document.write("<option value=\"didyougoogle\">did you google that comeback?</option>");
-                document.write("<option value=\"idk\">i dont know</option>");
-                document.write("<option value=\"ikr\">i know right</option>");
-                document.write("<option value=\"ilikeyou\">i like you</option>");
-                document.write("<option value=\"iloveyou\">i love you</option>");
-                document.write("<option value=\"ithinkyoure\">i think youre attractive</option>");
-                document.write("<option value=\"holdmybeerwatchthis\">hold my beer, watch this</option>");
-                document.write("<option value=\"idratherplaywithmycat\">id rather play with my cat</option>");
-                document.write("<option value=\"ilikeyou\">i like you</option>");
-                document.write("<option value=\"linkisbusted\">that link is busted/option>");
-                document.write("<option value=\"look it up\">look it up/option>");
-                document.write("<option value=\"lol\">LOL</option>");
-                document.write("<option value=\"nice\">nice</option>");
-                document.write("<option value=\"nuhuh\">Nuh UH!</option>");
-                document.write("<option value=\"ok\">ok</option>");
-                document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
-                document.write("<option value=\"sarcastic\">sarcastic clap</option>");
-                document.write("<option value=\"silence\">silence for effect</option>");
-                document.write("<option value=\"vague\">vague hipster comment</option>");
-                document.write("<option value=\"witty\">witty comeback</option>");
-                document.write("<option value=\"omg\">OMG</option>");
-                document.write("<option value=\"picsoritdidnthappen\">pics or it didnt happen</option>");
-                document.write("<option value=\"stfu\">STFU</option>");
-                document.write("<option value=\"thatlinkisrad\">that link is rad</option>");
-                document.write("<option value=\"thatscool\">thats cool</option>");
-                document.write("<option value=\"thatsucks\">that sucks</option>");
-                document.write("<option value=\"thatscool\">thats cool</option>");
-                document.write("<option value=\"thatswhatshesaid\">thats what she said</option>");
-                document.write("<option value=\"whatever\">whatever</option>");
-                document.write("<option value=\"youinspireme\">you inspire me</option>");
-                document.write("<option value=\"youlookverynicetoday\">you look very nice today</option>");
-                document.write("<option value=\"yourock\">you rock</option>");
-                document.write("<option value=\"youropinioniswrong\">your opinion is wrong</option>");
-                document.write("<option value=\"yousuck\">you suck</option>");
-                document.write("<option value=\"youknowyourearealupperson\">youre a real UP person</option>");
-                document.write("<option value=\"youremybestfriend\">youre my best friend</option>");
-                document.write("<option value=\"youreworkingthatoutfitgirl\">youre working that outfit girl</option>");
-                document.write("<option value=\"yourewrong\">youre wrong</option>");
+                writeAutoResponderOptions();
                 document.write("</select>");
                 document.write("<button>SEND</button>");
             document.write("</form>");
@@ -71,17 +30,39 @@ document.write("<table>");
     
 }
 
+function writeChatControls(){
+    document.write("<form action=\"\">");
+    document.write("<input id=\"chatClientUser\" placeholder=\"your alias\"/>");
+    document.write("<input id=\"chatClientMessage\" size=\"35\" placeholder=\"type message\" />");
+    document.write("<select id=\"chatClientAutoResponder\" size=1 >");
+    writeAutoResponderOptions();
+    document.write("</select>");
+    document.write("<button>SEND</button>");
+    document.write("</form>");
+}
+
+function writeChatMessagesDiv(){
+    document.write("<div id=\"messagesdiv\"></div>");
+}
 
 //WRITECHAT - this function writes the Stuffed Animal War style chat  to the page
 function writeChat(){
     document.write("<table>");
         document.write("<tr>");
         document.write("<td>");
-            document.write("<form action=\"\">");
-                document.write("<input id=\"chatClientUser\" placeholder=\"your alias\"/>");
-                document.write("<input id=\"chatClientMessage\" size=\"35\" placeholder=\"type message\" />");
-                document.write("<select id=\"chatClientAutoResponder\" size=1 >");
-                document.write("<option value=\"blank\" selected>--autoresponse--</option>");
+        writeChatControls();
+        document.write("</td>");
+        document.write("</tr>");
+        document.write("<tr>");
+        document.write("<td>");
+        writeChatMessagesDiv();
+        document.write("</td>");
+        document.write("</tr>");
+    document.write("</table>");
+}
+
+function writeAutoResponderOptions(){
+    document.write("<option value=\"blank\" selected>--autoresponse--</option>");
                 document.write("<option value=\"wink\">;)</option>");
                 document.write("<option value=\"smiley\">:)</option>");
                 document.write("<option value=\"bigsmiley\">:D</option>");
@@ -123,15 +104,6 @@ function writeChat(){
                 document.write("<option value=\"youremybestfriend\">youre my best friend</option>");
                 document.write("<option value=\"youreworkingthatoutfitgirl\">youre working that outfit girl</option>");
                 document.write("<option value=\"yourewrong\">youre wrong</option>");
-                document.write("</select>");
-                document.write("<button>SEND</button>");
-            document.write("</form>");
-        document.write("</td>");
-        document.write("</tr>");
-        document.write("<tr>");
-        document.write("<td><div id=\"messagesdiv\"></div></td>");
-        document.write("</tr>");
-    document.write("</table>");
 }
 
 //WRITEAUDIOPLAYER - this function writes the Stuffed Animal War style audio player to the page
@@ -288,4 +260,26 @@ function writeVideoPlayerDropdownVideos(){
     document.write("<option value=\"http://jaemzware.org/media/randommp4/sk8cretewall.mp4\">Sk8crete or Die</option>");
     document.write("<option value=\"http://jaemzware.org/media/randommp4/babysessionstruevalue.mp4\">True Value Baby Sessions 2001</option>");
     document.write("<option value=\"http://jaemzware.org/media/skate/fortboise201508.mp4\">Jim, Fort Boise August 2015</option>");
+}
+
+function writeVideoPlayerWithChat(){
+    document.write("<table>");
+    document.write("<tr>");
+    document.write("<td>");
+    document.write("<video id=\"jaemzwaredynamicvideoplayer\" poster=\"http://jaemzware.org/media/randompng/tv.png\" width=\"640\" height=\"480\" controls=\"controls\" preload=\"metadata\" title=\"jaemzwareTV\">");
+    document.write(" browser doesn't support mp4 video. use chrome.");
+    document.write("<source src=\"http://jaemzware.org/media/skate/roxhillbacksideollieline201508.mp4\" type=\"video/mp4\" id=\"jaemzwaredynamicvideosource\">");
+    document.write("</video>");
+    document.write("</td>");
+    document.write("</tr>");
+    document.write("<td>");
+    document.write("<select id=\"selectvideos\">");
+
+    writeVideoPlayerDropdownVideos();
+    
+    document.write("</select>");
+    document.write("</td>");
+    document.write("<td></td>");
+    document.write("</tr>");
+    document.write("</table>");
 }
