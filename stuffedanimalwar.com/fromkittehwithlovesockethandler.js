71889fcb185328a4921c6765d37800bafb87a1a8
JAMES ARASIM <jaemzware@hotmail.com>
2016-08-22 05:02:02 -0700

    kitteh

--- stuffedanimalwar.com/fromkittehwithlovesockethandler.js
+++ stuffedanimalwar.com/fromkittehwithlovesockethandler.js
@@ -1,7 +1,3 @@
-/* STUFFED ANIMAL WAR - jaemzware.org - 2015*/
-//REQUIRES STUFFED ANIMAL WAR MECHANICS FOR SOCKET HANDLER BASE
-$.getScript('stuffedanimalwarmechanics.js', function()
-{
 //REQUIRES UTILITIES FOR SOCKETHANDLER BASE
 $.getScript('utilities.js', function()
 {
@@ -17,9 +13,8 @@ $.getScript('sockethandler.js', function()
     var socket = io();
     initializeCommonVars(socket,masterAlias,unspecifiedAlias);
     initializeChatSocketHandler(socket,chatSocketEvent);
-    initializeTapSocketHandler(socket,tapSocketEvent);
 });
 });
-});
+
 
 
